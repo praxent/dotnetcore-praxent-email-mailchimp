9faa77ed9039619a1d6d68fb1f07daca22fe03f4
johnbrunningsatpraxent <john.brunnings@praxent.com>
2020-08-06 22:33:51 -0500

    Added initial Project.

---
+++ Praxent.Email.Mailchimp/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace Praxent.Email.Mailchimp
+{
+    public static class Bootstrapper
+    {
+        public static ContainerBuilder ExtendEmailSystemWithMailChimp(this ContainerBuilder builder)
+        {
+            builder.RegisterType<MailChimpTransactionalEmailManager>().As<ITransactionalEmailManager>();
+
+            return builder;
+        }
+    }
+}
---
+++ Praxent.Email.Mailchimp/MailChimpTransactionalEmailManager.cs
@@ -0,0 +1,34 @@
+﻿using Mandrill.Model;
+using Praxent.API.Client.Mailchimp;
+using Praxent.Email.Model;
+
+namespace Praxent.Email.Mailchimp
+{
+    public class MailChimpTransactionalEmailManager : ITransactionalEmailManager
+    {
+        private readonly IMandrillApiClient _mandrillApiClient;
+
+        public MailChimpTransactionalEmailManager(IMandrillApiClient mandrillApiClient)
+        {
+            _mandrillApiClient = mandrillApiClient;
+        }
+
+        public void SendTransactionalEmail(TransactionalEmail transactionalEmail)
+        {
+            var message = new MandrillMessage()
+            {
+                FromEmail = transactionalEmail.FromEmail,
+                ReplyTo = transactionalEmail.ReplyTo,
+                Subject = transactionalEmail.Subject
+            };
+
+            message.AddTo(transactionalEmail.EmailAddress);
+
+            foreach (var key in transactionalEmail.MergeValuesForTemplate.Keys)
+            {
+                message.AddRcptMergeVars(transactionalEmail.EmailAddress, key, transactionalEmail.MergeValuesForTemplate[key]);
+            }
+            _mandrillApiClient.SendTemplateAsync(message, transactionalEmail.TemplateName);
+        }
+    }
+}
